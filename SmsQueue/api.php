345a44526a5faad1bb96e4bd0810e3d406d27e3b
hikarutech <90462873+hikarutech@users.noreply.github.com>
2021-09-13 09:27:36 +0800

    Update api.php
    formating

--- SmsQueue/api.php
+++ SmsQueue/api.php
@@ -5,30 +5,24 @@ require_once __DIR__ . '/src/functions.php';
 
 use Siahow\SmsQueue\Sms;
 
-if(isset($_POST['c'])){
-
-	$message = new Sms();
+if (isset($_POST['c'])) {
+    $message = new Sms();
 	
-	if($_POST['c'] == 'createMessage'){
-		$msg_id = $message->createMessage($_POST['message']);
-		echo $msg_id;
-	}
-	elseif($_POST['c'] == 'sendMessage'){
-		$result = $message->sendMessage();
-		echo $result;		
-	}
-	elseif($_POST['c'] == 'totalMessage'){
-		$result = $message->totalMessage();
-		echo $result;				
-	}
-	elseif($_POST['c'] == 'getAllMessage'){
-		$result = $message->getAllMessage();
-		echo $result;			
-	}	
-	else{
-		
-	}
-}
-else{
-	echo "Error";
+	  if ($_POST['c'] == 'createMessage') {
+	  	  $msg_id = $message->createMessage($_POST['message']);
+	  	  echo $msg_id;
+	  } elseif ($_POST['c'] == 'sendMessage') {
+	  	  $result = $message->sendMessage();
+	  	  echo $result;		
+	  } elseif($_POST['c'] == 'totalMessage') {
+	  	  $result = $message->totalMessage();
+	  	  echo $result;				
+	  } elseif($_POST['c'] == 'getAllMessage') {
+	  	  $result = $message->getAllMessage();
+	  	  echo $result;			
+	  }	else {
+	  	  // do nothing
+	  }
+} else {
+    echo "Error";
 }
